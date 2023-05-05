56295bd7c6180158fa96b13075de0f8563eacdef
Aman A <amanag.11@gmail.com>
2023-05-05 10:47:40 -0400

    add multiple gpus

--- slurm-train.sh
+++ slurm-train.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -J train
+#SBATCH -J hpass_msrm
 #SBATCH -p gpu
 #SBATCH -A r00060
 #SBATCH -e logs/filename_%j.err
 #SBATCH -o logs/filename_%j.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=amanagar@iu.edu
-#SBATCH --gpus-per-node=v100:1
+#SBATCH --gpus-per-node=v100:4
 #SBATCH --nodes=1
 #SBATCH --time=40:00:00
 #SBATCH --mem=196G
@@ -16,4 +16,5 @@
 module load anaconda/python3.8
 
 #Run your program
-python train.py --img 400 --batch 64 --epochs 600 --data dataset-train/TreeThrow.yaml --name yolov5 --augment --cache 
+python -m torch.distributed.run --nproc_per_node 4 train.py --img 400 --batch 256 --epochs 600 --data dataset-train/TreeThrow.yaml --name hpass_msrm --augment --cache --weights '' --cfg yolov5m.yaml --device 0,1,2,3
+# python train.py --img 400 --batch 64 --epochs 600 --data dataset-train/TreeThrow.yaml --name slope_msrm --augment --cache --cfg yolov5s.yaml
--- slurm-val.sh
+++ slurm-val.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J val
+#SBATCH -J detect
 #SBATCH -p gpu
 #SBATCH -A r00060
 #SBATCH -e logs/filename_%j.err
@@ -16,5 +16,7 @@
 module load anaconda/python3.8
 
 #Run your program
-python val.py --weights runs/train/yolov5/weights/best.pt --data dataset-test/TreeThrow.yaml --img 400 --name brown_county_test --save-txt --task test --conf-thres 0.1 --single-cls --save-conf
+python detect.py --weights runs/train/yolov5m-scratch/weights/best.pt --source dataset-train/images/ --img 400 --name detect_test --conf-thres 0.1 --max-det 300 --save-txt --save-conf --nosave
+# python val.py --weights runs/train/yolov5m-scratch/weights/best.pt --data dataset-train/TreeThrow.yaml --img 400 --name test-augha --save-txt --task test --conf-thres 0.1 --single-cls --save-conf --augment --half
 
+# python detect.py --weights runs/train/yolov5m-scratch/weights/best.pt --source dataset-train/images/ --img 400 --name detect_test --conf-thres 0.32 --max-det 300 --save-txt --save-conf --nosave
